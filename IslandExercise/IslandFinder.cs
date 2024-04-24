3d77f75a95c83fb281a4d5af02e3f446461df78b
Julian Osinski <osinski.julek@gmail.com>
2024-04-24 02:58:20 +0200

    Project files added

---
+++ IslandExercise/IslandFinder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslandExercise
+{
+    internal class IslandFinder
+    {
+        int[,] _mapMatrix;
+        public IslandFinder(int[,] mapMatrix)
+        {
+            _mapMatrix = mapMatrix;
+        }
+
+        public int NumOfIslandDFS()
+        {
+            int numOfFoundedIsland = 0;
+            for (int i = 0;  i < _mapMatrix.GetLength(0); i++)
+            {
+                for(int j = 0;  j < _mapMatrix.GetLength(1); j++)
+                {
+                    if (_mapMatrix[i, j] == 1)
+                    {
+                        DFSHelper(i,j);
+                        numOfFoundedIsland++;
+                    }
+                }
+            }
+            return numOfFoundedIsland;
+        }
+        private void DFSHelper(int rowPos, int columnPos) 
+        {
+            //base case
+            if(!IsInBound(rowPos, columnPos) || _mapMatrix[rowPos,columnPos]!=1) {
+                return;
+            }
+            _mapMatrix[rowPos, columnPos] = 2;//mark as visited
+
+            DFSHelper(rowPos, columnPos+1); //go right
+            DFSHelper(rowPos+1, columnPos); //go down
+            DFSHelper(rowPos, columnPos - 1); //go left
+            DFSHelper(rowPos-1, columnPos); // go up
+        }
+        private bool IsInBound(int rowPos, int columnPos)
+        {
+            if(rowPos>=0&&rowPos<_mapMatrix.GetLength(0) && columnPos>=0&& columnPos<_mapMatrix.GetLength(1)) 
+            { 
+                return true; 
+            }
+            return false;
+        }
+    }
+}
---
+++ IslandExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace IslandExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] map =
+            {
+                {0,0,1,1,0},
+                {0,1,1,0,0},
+                {0,0,1,0,1},
+                {1,0,0,0,1},
+                {1,0,0,1,1}
+            };
+            IslandFinder islandFinder = new IslandFinder(map);
+            Console.WriteLine("Num of Island found: {0}", islandFinder.NumOfIslandDFS());
+        }
+    }
+}
